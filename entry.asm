578c3c40ed7880aaf37ef8e8d47316824d36349a
Hamer1224 <138596167+Hamer1224@users.noreply.github.com>
2026-02-06 10:56:52 +0000

    om3 os

--- entry.asm
+++ entry.asm
@@ -1,10 +1,10 @@
 [bits 32]
-[extern _kmain]     ; Look for C kernel function
+[extern kmain]     ; Look for C kernel function
 
 global start
 global _start       ; Export BOTH names so the linker definitely finds one
 
 start:
 _start:
-    call _kmain     ; Jump to C
+    call kmain     ; Jump to C
     jmp $           ; Hang if C returns
